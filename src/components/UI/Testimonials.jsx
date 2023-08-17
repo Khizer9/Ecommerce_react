a491bfa4d51f244fbb09fedd4c464be9248e99fa
Khizer Ali <khizer358@gmail.com>
2023-08-17 14:17:36 +0500

    added some pages

--- src/components/UI/Testimonials.jsx
+++ src/components/UI/Testimonials.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Fade } from 'react-reveal'
+import khizerImageTestimonail from '../Assets/khizer.jpg';
+import demoImageTestimonail from '../Assets/feed5.png';
 
 const Testimonials = () => {
   return (
@@ -16,7 +18,7 @@ const Testimonials = () => {
           </svg>
           <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
           <a class="inline-flex items-center">
-            <img alt="testimonial" src="https://dummyimage.com/106x106" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+            <img alt="testimonial" src={khizerImageTestimonail} class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
             <span class="flex-grow flex flex-col pl-4">
               <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
               <span class="text-gray-500 text-sm">UI DEVELOPER</span>
@@ -31,7 +33,7 @@ const Testimonials = () => {
           </svg>
           <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
           <a class="inline-flex items-center">
-            <img alt="testimonial" src="https://dummyimage.com/107x107" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+            <img alt="testimonial" src={demoImageTestimonail} class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
             <span class="flex-grow flex flex-col pl-4">
               <span class="title-font font-medium text-gray-900">Alper Kamu</span>
               <span class="text-gray-500 text-sm">DESIGNER</span>
